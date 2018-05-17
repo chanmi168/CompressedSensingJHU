b0c352d95b943bbe56c0d64d10c307090647af82
klai10 <wei09800533@gmail.com>
2018-05-16 23:07:43 -0400

    dialation

---
+++ dialation/pretrained/download_cityscapes.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+curl -O http://dl.yf.io/dilation/models/dilation10_cityscapes.caffemodel
---
+++ dialation/pretrained/download_pascal_voc.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+curl -O http://dl.yf.io/dilation/models/dilation8_pascal_voc.caffemodel
